5f9eaab7092ce3b0111ce0910a1d031998f6427f
MarianoBF <mariano.b.fernandez@gmail.com>
2021-04-17 14:11:17 -0300

    basic game flow test

--- cypress/integration/inicio_spec.js
+++ cypress/integration/inicio_spec.js
@@ -1,13 +1,24 @@
-describe('Inicio', () => {
-
+describe("Inicio", () => {
   beforeEach(() => {
-    cy.visit('localhost:5500')
-  })
-  
-    it('Encuentra botonera e inicia', () => {
-      cy.get('.botonera').should('be.visible')
-        cy.get('.botonera').contains('Iniciar partido').click().wait(1000)
-        cy.get('#repartir').should('not.be.disabled').click()
-    })
+    cy.visit("localhost:5500");
+  });
 
-  })
+  it("Encuentra botonera e inicia", () => {
+    
+    cy.get(".botonera").should("be.visible");
+    cy.get(".botonera").contains("Iniciar partido").wait(1500).click();
+    cy.get("#repartir").should("not.be.disabled").wait(1500).click();
+    cy.get("#puntuar").should("not.be.disabled").wait(1500).click();
+    cy.get("#cerrar").should("not.be.disabled").wait(1500).click();
+
+    cy.get("#repartir").should("not.be.disabled").wait(1500).click();
+    cy.get("#puntuar").should("not.be.disabled").wait(1500).click();
+    cy.get("#cerrar").should("not.be.disabled").wait(1500).click();
+
+    cy.get("#repartir").should("not.be.disabled").wait(1500).click();
+    cy.get("#puntuar").should("not.be.disabled").wait(1500).click();
+    cy.get("#cerrar").should("not.be.disabled").wait(1500).click()
+    cy.on('window:alert', message => expect(message).to.match(/El jugador/));
+
+  });
+});
